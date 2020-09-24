98ab165e256bff25fba350a2f013c2b36ecd17df
MP <codermp.26@gmail.com>
2020-09-25 00:32:05 +0530

    add env var

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-var express = require("express"),
-	app = express(),
-	bodyParser = require("body-parser"),
-	mongoose = require("mongoose"),
-	flash = require("connect-flash"),
-	User = require("./models/user"),
-	Train = require("./models/train"),
-	registerRoutes = require("./routes/register"),
-	displayRoutes = require("./routes/display"),
-	indexRoutes = require("./routes/index");
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  User = require("./models/user"),
+  Train = require("./models/train"),
+  registerRoutes = require("./routes/register"),
+  displayRoutes = require("./routes/display"),
+  indexRoutes = require("./routes/index");
+require("dotenv").config();
 
-mongoose.connect(process.env.DATABASEURL ,{useNewUrlParser: true,useUnifiedTopology: true});
-//mongoose.connect("mongodb://localhost:27017/trainectApp", {useNewUrlParser: true,useUnifiedTopology: true});
 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect(process.env.DATABASEURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(flash());
 app.use(express.static("public"));
@@ -21,6 +25,6 @@ app.use(registerRoutes);
 app.use(displayRoutes);
 app.use(indexRoutes);
 
-app.listen(process.env.PORT, function(){
-	console.log("Trainect server started");
+app.listen(process.env.PORT, function () {
+  console.log("Trainect server is up !!");
 });
